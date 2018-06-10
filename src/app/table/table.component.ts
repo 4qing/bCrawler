b88f48dc006dd3526113027cf381a4dd028b245f
Qing <291276560@qq.com>
2018-06-10 21:12:19 +0800

    fix a mistake

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -7,8 +7,9 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit,AfterViewInit {
   @Input() conn: string;
+  @ViewChild('basicTable') basicTable:ElementRef;
   loding :boolean = true;
   dataSet = [];
   constructor(private http:HttpClient) {
@@ -16,11 +17,15 @@ export class TableComponent implements OnInit {
    }
 
   ngOnInit() {
+    
     this.http.get(`http://localhost:8080/${this.conn}.json`).subscribe((res:any)=> {
-      console.log(this.conn);
+
         this.loding = false;
         this.dataSet=res;
       });
   }
-
+  ngAfterViewInit() {
+    let table = this.basicTable.nativeElement;
+      console.log("table:" + table);
+  }
 }
